f2688023be446ccb84d54d1adf4e7948309080b4
numberx <shekooha696@gmail.com>
2022-03-18 20:11:51 +0330

    added product_list to urls

--- market/urls.py
+++ market/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     # products urls
     path('product/insert/', views.product_insert, name='product_insert'),
+    path('product/list/', views.product_list, name='product_list'),
     # TODO: insert other url paths
-    # path(...
-    # path(...
-    # path(...
 ]
