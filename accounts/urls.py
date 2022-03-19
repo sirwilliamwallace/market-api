2ee03014b02b7a1129b5dc7160a2d0651539928c
numberx <shekooha696@gmail.com>
2022-03-19 09:41:26 +0330

    Added Edit user

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('customer/register/', views.register_customer, name='index'),
     path('customer/list/', views.list_customers , name='list_users'),
     path('customer/<int:customer_id>/', views.customer_detail , name='list_users'),
+    path('customer/<int:customer_id>/edit/', views.customer_edit , name='edit_users'),
 ]
 
